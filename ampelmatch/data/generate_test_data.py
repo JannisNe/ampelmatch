d68bd396e06e07d518185c76868aef277abe8191
JannisNe <necker@physik.hu-berlin.de>
2025-01-22 19:04:55 +0100

    rename

--- ampelmatch/data/generate_test_data.py
+++
@@ -1,8 +0,0 @@
-import logging
-from ampelmatch.data.dataset import generate_test_data
-
-
-if __name__ == '__main__':
-    logging.getLogger("ampelmatch").setLevel("INFO")
-    logging.getLogger("ampelmatch").info("Generating test data")
-    list(generate_test_data())
