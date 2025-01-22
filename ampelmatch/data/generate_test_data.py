faf6d8524bc92eebee45b188d681d5f44f1227c1
JannisNe <necker@physik.hu-berlin.de>
2025-01-22 18:26:45 +0100

    add logging

--- ampelmatch/data/generate_test_data.py
+++ ampelmatch/data/generate_test_data.py
@@ -4,4 +4,5 @@ from ampelmatch.data.dataset import generate_test_data
 
 if __name__ == '__main__':
     logging.getLogger("ampelmatch").setLevel("INFO")
+    logging.getLogger("ampelmatch").info("Generating test data")
     list(generate_test_data())
