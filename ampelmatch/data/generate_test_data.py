9969d05608926cad34ef6208c502291cbbc0a222
JannisNe <necker@physik.hu-berlin.de>
2025-01-22 15:37:36 +0100

    add from_pointings

--- ampelmatch/data/generate_test_data.py
+++ ampelmatch/data/generate_test_data.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 from shapely import geometry
 from astropy.time import Time
+import hashlib
+from pathlib import Path
 
 from ampelmatch.data.positional_uncertainty import GaussianUncertainty
 from ampelmatch.data.positional_survey import PositionalGridSurvey
@@ -17,51 +19,65 @@ def generate_transient_sample():
     snia = skysurvey.SNeIa().draw(10_000)
     snia.to_csv("snia.csv")
 
+field1 = {'ra': +150.11916667, 'dec': +2.20583333}
+survey_params = {
+    "survey1": {
+        "fields": {0: field1},
+        'fov': 8,
+        'gain': 1,
+        'zp': 30,
+        'skynoise': 150,
+        'time_min': "2020-03-01",
+        'time_max': "2020-04-01",
+        'bands': ['ztfg'],
+        "size": 10_000
+    },
+    "survey2": {
+        "fields": {0: {'ra': field1["ra"] + 2, 'dec': field1["dec"] + 0.4}},
+        'fov': 5,
+        'gain': 1,
+        'zp': 30,
+        'skynoise_mean': 150,
+        'time_min': "2020-03-01",
+        'time_max': "2020-04-01",
+        'bands': ['ztfg'],
+        'size': 10_000
+    }
+}
+_one_degree_vertices = np.asarray([[-0.5, -0.5], [0.5, -0.5], [0.5, 0.5], [-0.5, 0.5]])
 
-def generate_test_observations():
+def get_test_observations(survey_name: str):
     """
     generate observations by two overlapping mock surveys that observe one field each
     :return:
     """
 
-    _one_degree_vertices = np.asarray([[-0.5, -0.5], [0.5, -0.5], [0.5, 0.5], [-0.5, 0.5]])
+    params = survey_params[survey_name]
+    h = hashlib.md5().hexdigest(params)
+    fname = Path(f"{survey_name}_{h}.csv")
+    if not fname.exists():
+        logger.info(f"generating {survey_name} observations")
+        size = params["size"]
+        data = {}
+        data["fieldid"] = np.random.choice(list(params["fields"].keys()), size=size)
+        data["gain"] = params["gain"]
+        data["zp"] = params["zp"]
+        data["skynoise"] = np.random.normal(loc=params["skynoise_mean"], scale=20, size=size)
+        data["mjd"] = np.random.uniform(Time(params["time_min"]).mjd, Time(params["time_max"]).mjd, size=size)
+        data["band"] = np.random.choice(params["bands"], size=size)
+        data = pandas.DataFrame.from_dict(data)
+        logger.info(f"generated {size} observations for survey {survey_name}")
+        data.to_csv(fname)
+        logger.info(f"saved to {fname}")
+    return pd.read_csv(fname, index_col=0)
 
-    # survey 1
-    field1 = {'ra': +150.11916667, 'dec': +2.20583333}
-    vertices_camera1 = _one_degree_vertices * 8
-    footprint1 = geometry.Polygon(vertices_camera1)
-    size1 = 10_000
-    data1 = {}
-    data1["fieldid"] = np.array([0] * size1)
-    data1["gain"] = 1
-    data1["zp"] = 30
-    data1["skynoise"] = np.random.normal(loc=150, scale=20, size=size1)
-    data1["mjd"] = np.random.uniform(Time("2020-03-01").mjd, Time("2020-04-01").mjd, size=size1)
-    data1["band"] = np.array(["ztfg"] * size1)
-    data1 = pd.DataFrame.from_dict(data1)
-    logger.info(f"Generated {size1} observations for survey 1")
-    data1.to_csv("survey1.csv")
-    logger.info(f"Saved survey 1 to survey1.csv")
 
-    # survey 2
-    field2 = {'ra': field1["ra"] + 2, 'dec': field1["dec"] + 0.4}
-    vertices_camera2 = _one_degree_vertices * 5
-    footprint2 = geometry.Polygon(vertices_camera2)
-    size2 = 10_000
-    data2 = {}
-    data2["fieldid"] = np.array([0] * size2)
-    data2["gain"] = 1
-    data2["zp"] = 20
-    data2["skynoise"] = np.random.normal(loc=150, scale=20, size=size2)
-    data2["mjd"] = np.random.uniform(Time("2020-03-01").mjd, Time("2020-04-01").mjd, size=size2)
-    data2["band"] = np.array(["atlaso"] * size2)
-    data2 = pd.DataFrame.from_dict(data2)
-    logger.info(f"Generated {size2} observations for survey 2")
-    data2.to_csv("survey2.csv")
-    logger.info(f"Saved survey 2 to survey2.csv")
+def generate_test_surveys():
+    for survey_name in survey_params:
+        survey = PositionalGridSurvey()
 
 
 if __name__ == "__main__":
     logging.getLogger("ampelmatch").setLevel(logging.DEBUG)
     # generate_test_data()
-    generate_test_observations()
+    ge()
--- ampelmatch/data/positional_survey.py
+++ ampelmatch/data/positional_survey.py
@@ -9,9 +9,15 @@ logger = logging.getLogger(__name__)
 
 class PositionalGridSurvey(skysurvey.GridSurvey):
 
-    def __init__(self, survey, uncertainty: BaseUncertainty):
-        super().__init__(survey)
+    def __init__(self, uncertainty: BaseUncertainty, data=None, fields=None):
+        super().__init__(data, fields)
         self.uncertainty = uncertainty
+
+    @classmethod
+    def from_pointings(cls, data, footprint=None, moc=None, rakey="ra", deckey="dec", uncertainty: BaseUncertainty = None):
+        _survey = super().from_pointings(data, footprint, moc, rakey, deckey)
+        _survey.uncertainty = uncertainty
+        return _survey
 
     def draw_positions(self, n: int, truth: tuple[float, float]) -> list[tuple[float, float]]:
         return self.uncertainty.draw_position(n, truth)
