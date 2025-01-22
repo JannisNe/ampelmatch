3d647543d917cfc995bb030172c66f4494f9b878
JannisNe <necker@physik.hu-berlin.de>
2025-01-22 17:56:51 +0100

    use ztfr to stay inside snia template lambda range

--- ampelmatch/data/surveys.py
+++ ampelmatch/data/surveys.py
@@ -27,7 +27,7 @@ survey_params = {
         'skynoise_mean': 150,
         'time_min': "2020-03-01",
         'time_max': "2020-04-01",
-        'bands': ['ztfg'],
+        'bands': ['ztfr'],
         "size": 10_000,
         "uncertainty": {
             "type": "GaussianUncertainty",
