472822b6cafe7292be668c8e24a06d08cd3a0b4a
JannisNe <necker@physik.hu-berlin.de>
2025-01-22 15:47:57 +0100

    add generating

--- ampelmatch/data/positional_uncertainty.py
+++ ampelmatch/data/positional_uncertainty.py
@@ -8,8 +8,26 @@ logger = logging.getLogger(__name__)
 
 
 class BaseUncertainty:
+    registry = {}
+
     def draw_position(self, n: int, truth: tuple[float, float]) -> list[tuple[float, float]]:
         ...
+
+    @classmethod
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        if cls.__name__ in BaseUncertainty.registry:
+            raise ValueError(f"Duplicate uncertainty class name: {cls.__name__}")
+        BaseUncertainty.registry[cls.__name__] = cls
+
+    @classmethod
+    def from_dict(cls, data: dict) -> 'BaseUncertainty':
+        if "type" not in data:
+            raise ValueError("Uncertainty type not provided")
+        _type = data.pop("type")
+        if _type not in BaseUncertainty.registry:
+            raise ValueError(f"Uncertainty type not recognized: {_type}")
+        return BaseUncertainty.registry[_type](**data)
 
 
 class GaussianUncertainty(BaseUncertainty):
