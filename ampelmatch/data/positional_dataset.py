242a255766541b6ffcdbca6df446754bb954da4c
JannisNe <necker@physik.hu-berlin.de>
2025-01-23 10:33:38 +0100

    better inheritance

--- ampelmatch/data/positional_dataset.py
+++ ampelmatch/data/positional_dataset.py
@@ -18,7 +18,7 @@ class PositionalDataset(skysurvey.DataSet):
         assert hasattr(survey, 'uncertainty'), "Survey must have uncertainties"
         unc = survey.uncertainty
         assert isinstance(unc, BaseUncertainty), "uncertainty must be a subclass of BaseUncertainty"
-        lc_out, fieldids_indexes = skysurvey.DataSet._realize_survey_kindtarget_lcs(
+        lc_out, fieldids_indexes = super(PositionalDataset, PositionalDataset)._realize_survey_kindtarget_lcs(
             targets, survey, template, template_prop, nfirst, incl_error, client, discard_bands
         )
         for c in unc.POSITION_KEYS + unc.PARAMETER_KEYS:
