6671d44fcff4b12a6e7bafe49beb053533bdb97e
JannisNe <necker@physik.hu-berlin.de>
2025-01-24 14:16:43 +0100

    add plotter

--- ampelmatch/data/make_plots.py
+++ ampelmatch/data/make_plots.py
@@ -3,17 +3,21 @@ import cartopy.crs as ccrs
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.collections import PolyCollection
-from ampelmatch.data.dataset import generate_test_data
+
+from ampelmatch.data.config import DatasetConfig
+from ampelmatch.data.plotter import Plotter
 
 
 logger = logging.getLogger("ampelmatch.data.make_plots")
+
 
 fields = {
     0: {'ra': +150.11916667, 'dec': +2.20583333},
     1: {'ra': +10.11916667, 'dec': -2.60583333}
 }
-survey_params = {
-    "survey1": {
+survey_params = [
+    {
+        "name": "survey1",
         "fields": fields,
         'fov': 10,
         'gain': 1,
@@ -26,9 +30,10 @@ survey_params = {
         "uncertainty": {
             "type": "GaussianUncertainty",
             "sigma_arcsec": 1
-        }
+        },
     },
-    "survey2": {
+    {
+        "name": "survey2",
         "fields": {k: {'ra': v['ra'] + 0.1, 'dec': v['dec'] - 0.2} for k, v in fields.items()},
         'fov': 5,
         'gain': 1,
@@ -43,74 +48,23 @@ survey_params = {
             "sigma_arcsec": 3
         }
     }
-}
-transient_config = {
-    "SNeIa": {
+]
+transient_config = [
+    {
+        "name": "SNIa",
         "draw": 10_000,
         "zmax": 1
     }
+]
+dataset_config = {
+    "name": "test",
+    "surveys": survey_params,
+    "transients": transient_config
 }
 
 
 if __name__ == '__main__':
     logging.getLogger("ampelmatch").setLevel("INFO")
     logging.getLogger("ampelmatch").info("Generating test data")
-    d = list(generate_test_data())
-    n_det = [[ddd.get_ndetection() for ddd in dd] for dd in d]
-    max_i = [max(indet.index) for ndet in n_det for indet in ndet]
-
-    i = 0
-    n_plotted = 0
-    for j in range(len(d[0])):
-        while n_plotted < 3:
-            while not all([i in _ndet[j] for _ndet in n_det]):
-                i += 1
-            logger.info(f"Plotting target {i}")
-
-            fig, ax = plt.subplots()
-            for il, l in enumerate(d):
-                l[j].show_target_lightcurve(index=i, ax=ax, label=f"Survey {il}")
-            ax.legend()
-            fig.savefig(f"target_{j}_{i}.pdf")
-            plt.close()
-
-            fig, ax = plt.subplots()
-            for il, l in enumerate(d):
-                lc = l[j].get_target_lightcurve(index=i)
-                ax.scatter(lc["ra"], lc["dec"], label=f"Survey {il}")
-            ax.legend()
-            ax.set_aspect("equal")
-            fig.savefig(f"target_{j}_{i}_pos.pdf")
-            plt.close()
-
-            n_plotted += 1
-            i += 1
-
-        logger.info(f"Plotting sky coverage for transient {j}")
-        origin = 180
-        t = ccrs.PlateCarree(central_longitude=origin)
-        fig = plt.figure()
-        ax = fig.add_axes([0.15, 0.22, 0.75, 0.75], projection=ccrs.Mollweide())
-        for dddi, ddd in enumerate([dd[j] for dd in d]):
-            s = ddd.survey
-            data = s.get_fieldstat(stat="size", columns=None, incl_zeros=True, fillna=np.nan, data=None)
-            geodf = s.fields.copy()
-            xy = np.stack(geodf["geometry"].apply(lambda x: ((np.asarray(x.exterior.xy)).T)).values)
-            # correct edge effects
-            flag_egde = np.any(np.diff(xy, axis=1) > 300, axis=1)[:, 0]
-            xy[flag_egde] = ((xy[flag_egde] + origin) % 360 - origin)
-            geodf["xy"] = list(xy)
-            ax.add_collection(PolyCollection(
-                geodf["xy"], transform=t, ec=f"C{dddi}", label=f"Survey {dddi}", alpha=0.5, fc="none"
-            ))
-            targets = ddd.targets
-            det_ids = n_det[dddi][j].index
-            det = targets.data.loc[det_ids]
-            ax.scatter(det["ra"], det["dec"], transform=t, color=f"C{dddi}", s=1)
-
-        # ax.autoscale()
-        ax.set_global()
-        ax.legend()
-        ax.gridlines()
-        fig.savefig(f"sky_{j}.pdf")
-        plt.close()
+    c = DatasetConfig.model_validate(dataset_config)
+    Plotter(c).make_plots()
---
+++ ampelmatch/data/plotter.py
@@ -0,0 +1,84 @@
+import logging
+import cartopy.crs as ccrs
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.collections import PolyCollection
+import itertools
+from pathlib import Path
+
+from ampelmatch.data.config import DatasetConfig
+from ampelmatch.data.dataset import DatasetGenerator
+
+
+logger = logging.getLogger(__name__)
+
+
+class Plotter:
+    def __init__(self, config: DatasetConfig):
+        self.datasets = DatasetGenerator(config)
+        self.config = config
+        self.dir = Path(config.name)
+
+    def make_plots(self):
+        n_surveys = len(self.config.surveys)
+        for i, dsets in enumerate(itertools.batched(self.datasets, n_surveys)):
+            logger.info(f"Generating plots for dataset {i}")
+            n_det = [d.get_ndetection() for d in dsets]
+            fig, ax = self.sky_plot(dsets, n_det)
+            fn = self.dir / f"sky_coverage_{i}.pdf"
+            fig.savefig(fn)
+            logger.info(f"Saved sky coverage plot to {fn}")
+            plt.close()
+
+            indices = list(set.intersection(*[set(n.index) for n in n_det]))
+            for j in np.random.choice(indices, 10):
+                logger.info(f"Plotting target {j}")
+                fig, axs = self.lightcurve_plot(dsets, j)
+                fn = self.dir / f"lightcurve_{i}_{j}.pdf"
+                fig.savefig(fn)
+                logger.info(f"Saved lightcurve plot to {fn}")
+                plt.close()
+
+    @staticmethod
+    def lightcurve_plot(dsets, i):
+        fig, (ax1, ax2) = plt.subplots(nrows=2)
+        for il, l in enumerate(dsets):
+            l.show_target_lightcurve(index=i, ax=ax1, label=f"Survey {il}")
+        ax1.legend()
+        for il, l in enumerate(dsets):
+            lc = l.get_target_lightcurve(index=i)
+            ax2.scatter(lc["ra"], lc["dec"], label=f"Survey {il}")
+        ax2.legend()
+        ax2.set_aspect("equal")
+
+        return fig, (ax1, ax2)
+
+    @staticmethod
+    def sky_plot(dsets, n_det):
+        logger.info(f"Plotting sky coverage for transient {j}")
+        origin = 180
+        t = ccrs.PlateCarree(central_longitude=origin)
+        fig = plt.figure()
+        ax = fig.add_axes((0.15, 0.22, 0.75, 0.75), projection=ccrs.Mollweide())
+        for dddi, ddd in enumerate([dsets]):
+            s = ddd.survey
+            data = s.get_fieldstat(stat="size", columns=None, incl_zeros=True, fillna=np.nan, data=None)
+            geodf = s.fields.copy()
+            xy = np.stack(geodf["geometry"].apply(lambda x: ((np.asarray(x.exterior.xy)).T)).values)
+            # correct edge effects
+            flag_egde = np.any(np.diff(xy, axis=1) > 300, axis=1)[:, 0]
+            xy[flag_egde] = ((xy[flag_egde] + origin) % 360 - origin)
+            geodf["xy"] = list(xy)
+            ax.add_collection(PolyCollection(
+                geodf["xy"], transform=t, ec=f"C{dddi}", label=f"Survey {dddi}", alpha=0.5, fc="none"
+            ))
+            targets = ddd.targets
+            det_ids = n_det[dddi].index
+            det = targets.data.loc[det_ids]
+            ax.scatter(det["ra"], det["dec"], transform=t, color=f"C{dddi}", s=1)
+
+        # ax.autoscale()
+        ax.set_global()
+        ax.legend()
+        ax.gridlines()
+        return fig, ax
