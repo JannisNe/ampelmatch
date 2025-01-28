93115f1623b53a684569f2cb87a6b98f600b831f
JannisNe <necker@physik.hu-berlin.de>
2025-01-28 10:33:38 +0100

    add skyareas plot before generating data

--- ampelmatch/match/test_gaussian.py
+++ ampelmatch/match/test_gaussian.py
@@ -1,9 +1,12 @@
 import logging
 from pathlib import Path
+import matplotlib.pyplot as plt
 
 from ampelmatch.data.config import DatasetConfig
 from ampelmatch.data.dataset import DatasetGenerator
 from ampelmatch.data.plotter import Plotter
+from ampelmatch.data.transients import TransientGenerator
+from ampelmatch.data.surveys import SurveyGenerator
 from ampelmatch.match.gaussian import StreamMatch
 
 
@@ -14,6 +17,15 @@ if __name__ == '__main__':
     logging.getLogger("ampelmatch").setLevel("DEBUG")
     dset_config_fname = Path(__file__).parent / "test_sim.json"
     dset_config = DatasetConfig.model_validate_json(dset_config_fname.read_text())
+    for t, tc in zip(TransientGenerator(dset_config.transients), dset_config.transients):
+        surveys = list(SurveyGenerator(dset_config.surveys))
+        targets = [t for _ in surveys]
+        fig, ax = Plotter.sky_plot(surveys, targets, target_skyarea=tc.skyarea)
+        fname = Path(f"{tc.transient_type}_sky_coverage.pdf")
+        logger.info(f"Saving sky coverage plot to {fname}")
+        fig.savefig(fname)
+        plt.close()
+
     dsets = DatasetGenerator(dset_config)
     if any([not f.exists() for f in dsets.filenames]):
         DatasetGenerator(dset_config).write()
