d49923baff351e07e15033be3a62c57e0658817b
JannisNe <necker@physik.hu-berlin.de>
2025-02-10 18:40:09 +0100

    better sim

---
+++ ampelmatch/match/test_icecube.py
@@ -0,0 +1,68 @@
+import logging
+import ipdb
+from pathlib import Path
+import matplotlib.pyplot as plt
+
+from ampelmatch.data.config import DatasetConfig
+from ampelmatch.data.dataset import DatasetGenerator
+from ampelmatch.data.plotter import Plotter
+from ampelmatch.data.transients import TransientGenerator
+from ampelmatch.data.surveys import SurveyGenerator
+from ampelmatch.data.icecube_alert import IceCubeAlerts
+from ampelmatch.match.match import StreamMatch
+
+
+logger = logging.getLogger("ampelmatch.match.test_gaussian")
+
+
+if __name__ == '__main__':
+    logging.getLogger("ampelmatch").setLevel("DEBUG")
+    dset_config_fname = Path(__file__).parent / "test_sim_icecube.json"
+    dset_config = DatasetConfig.model_validate_json(dset_config_fname.read_text())
+    h = dset_config.get_hash()
+    for t, tc in zip(TransientGenerator(dset_config.transients), dset_config.transients):
+        surveys = list(SurveyGenerator(dset_config.surveys))
+        targets = [t for _ in surveys]
+        fig, ax = Plotter.sky_plot(surveys, targets, target_skyarea=tc.skyarea)
+        fname = Path(dset_config.name) / h / f"{tc.transient_type}_sky_coverage.pdf"
+        logger.info(f"Saving sky coverage plot to {fname}")
+        fname.parent.mkdir(exist_ok=True, parents=True)
+        fig.savefig(fname)
+        plt.close()
+
+    dsets = DatasetGenerator(dset_config)
+    if any([not f.exists() for f in dsets.filenames]):
+        DatasetGenerator(dset_config).write()
+        Plotter(dset_config).make_data_plots(n_lightcurves=10)
+
+    batched_fns = []
+    for i in range(1, dsets.n_transients + 1):
+        batched_fns.append(dsets.filenames[:i*dsets.n_surveys])
+
+    icecube_alerts = IceCubeAlerts()
+    icecube_alert_filename = Path("icecube_alerts.csv")
+    if not icecube_alert_filename.is_file():
+        icecube_alerts.load_data()
+        icecube_alerts.write_data(icecube_alert_filename)
+
+    for i, fns in enumerate(batched_fns):
+        logger.info(f"Matching batch {i}")
+        match_config = {
+            "name": f"{dset_config.name}/{h}/{i}",
+            "match_type": "icecube_contour",
+            "disc_radius_arcsec": None,
+            "plot": 30,
+            "nside": 128,
+            "primary_data": {
+                "filepath_or_buffer": fns[0],
+                "index_col": 0,
+            },
+            "match_data": [
+                {
+                    "filepath_or_buffer": icecube_alert_filename,
+                }
+            ]
+        }
+        match = StreamMatch.validate_python(match_config)
+        bayes_factors = match.match()
+        ipdb.set_trace()
