38dd3ccf0af63476313d608bd90a5d2a03cf10b2
JannisNe <necker@physik.hu-berlin.de>
2025-02-19 10:14:02 +0100

    add prior crosscheck

--- ampelmatch/match/test_gaussian.py
+++ ampelmatch/match/test_gaussian.py
@@ -1,20 +1,23 @@
+import importlib
 import logging
 from pathlib import Path
 
 import matplotlib.pyplot as plt
+import numpy as np
 from ampelmatch.data.config import DatasetConfig
 from ampelmatch.data.dataset import DatasetGenerator
-from ampelmatch.data.icecube_alert import IceCubeAlerts
 from ampelmatch.data.plotter import Plotter
 from ampelmatch.data.surveys import SurveyGenerator
 from ampelmatch.data.transients import TransientGenerator
-from ampelmatch.match.match import StreamMatch
+from ampelmatch.match import match
+
+match = importlib.reload(match)
 
 logger = logging.getLogger("ampelmatch.match.test_gaussian")
 
 
 if __name__ == "__main__":
-    logging.getLogger("ampelmatch").setLevel("DEBUG")
+    logging.getLogger("ampelmatch").setLevel("INFO")
     dset_config_fname = Path(__file__).parent / "test_sim.json"
     dset_config = DatasetConfig.model_validate_json(dset_config_fname.read_text())
     h = dset_config.get_hash()
@@ -39,21 +42,19 @@ if __name__ == "__main__":
     for i in range(1, dsets.n_transients + 1):
         batched_fns.append(dsets.filenames[: i * dsets.n_surveys])
 
-    icecube_alerts = IceCubeAlerts()
-    icecube_alert_filename = Path("icecube_alerts.csv")
-    if not icecube_alert_filename.is_file():
-        icecube_alerts.load_data()
-        icecube_alerts.write_data(icecube_alert_filename)
-
     for i, fns in enumerate(batched_fns):
         logger.info(f"Matching batch {i}")
         match_config = {
             "bayes_factor": {
-                "name": f"{dset_config.name}/{h}/{i}",
+                "name": f"{dset_config.name}/{h}/{i}_fine",
                 "match_type": "gaussian",
-                "plot": 30,
-                "nside": 128,
-                "primary_data": {"filepath_or_buffer": fns[0], "index_col": 0},
+                "plot": 10,
+                "nside": 1024,
+                "primary_data": {
+                    "filepath_or_buffer": fns[0],
+                    "index_col": 0,
+                    # "nrows": 700,
+                },
                 "match_data": [
                     {
                         "filepath_or_buffer": fn,
@@ -63,10 +64,31 @@ if __name__ == "__main__":
             },
             "prior": {
                 "name": "surface_density",
-                "nside": 1024,
-                "area_sqdg": dset_config.surveys[0].fov
-                * len(dset_config.surveys[0].fields),
+                "nside": 128,
+                "primary_data": {
+                    "filepath_or_buffer": fns[0],
+                    "index_col": 0,
+                    # "nrows": 700,
+                },
+                "match_data": [
+                    {
+                        "filepath_or_buffer": fn,
+                    }
+                    for fn in fns[1:]
+                ],
             },
         }
-        match = StreamMatch.model_validate(match_config)
-        probabilities = match.calculate_posteriors()
+        match = match.StreamMatch.model_validate(match_config)
+        prod = sum([len(d) for d in match.bayes_factor.match_data_df])
+        prios_values = [
+            match.prior(match.bayes_factor.primary_data_df.loc[dd])
+            for dd in match.bayes_factor.primary_data_df.index.unique()
+        ]
+        a = min(s.fov for s in dset_config.surveys) * (np.pi / 180) ** 2
+        print(
+            sum(prios_values)
+            # * prod
+            * (a / (4 * np.pi)) ** (1 - len(match.bayes_factor.match_data_df)),
+            prod,
+        )
+        # probabilities = match.calculate_posteriors()
